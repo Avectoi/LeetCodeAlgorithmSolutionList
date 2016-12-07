f87a0ff0fba6d00090a9b4dc7cea8b8b7862becd
Illyasviel <carson965853914@outlook.com>
2016-12-07 21:44:49 +0800

    add12

---
+++ LC/12. Integer to Roman.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def intToRoman(self, num):
+        c = [[""for i in range(4)]for j in range(10)]
+        c = [["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],  # 1
+             ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],  # 10
+             ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],  # 100
+             ["", "M", "MM", "MMM"]  # 1000
+             ]
+        s = ""
+        s = s+c[3][num / 1000 % 10]
+        s = s+c[2][num / 100 % 10]
+        s = s+c[1][num / 10 % 10]
+        s = s+c[0][num / 1 % 10]
+
+        return s
