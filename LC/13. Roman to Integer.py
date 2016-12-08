a5c2ead286bac4e022d7c7b9513d71c9d37032c4
Illyasviel <carson965853914@outlook.com>
2016-12-08 08:33:24 +0800

    change 13

--- LC/13. Roman to Integer.py
+++ LC/13. Roman to Integer.py
@@ -1,40 +1,29 @@
 def romanToInt(s):
-    c = [[""for i in range(4)]for j in range(10)]
-    c = [["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],  # 1
-         ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],  # 10
-         ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],  # 100
-         ["", "M", "MM", "MMM"]  # 1000
-         ]
     s_len = len(s)
-    sumnum = 0
-    for j in range(3, 0, -1):
-        if s.find(c[3][j]) != -1:
-            c_len = len(c[3][j])
-            s.find(c[3][j])
-            sumnum = 1000 * j
-            # print c_len
-            s = s[c_len: len(s)]
-            # print s#,sumnum
-            # print sumnum
+    sum = 0
+    for i in range(s_len - 1, -1, -1):#1,5,10,50,100,500,1000
+        if s[i] == 'I' and sum < 5:
+            sum += 1
+        elif s[i] == 'I' and sum >= 5:
+            sum -= 1
+        elif s[i] == 'V':
+            sum += 5
+        elif s[i] == 'X' and sum < 50:
+            sum += 10
+        elif s[i] == 'X' and sum >= 50:
+            sum -= 10
+        elif s[i] == 'L':
+            sum += 50
+        elif s[i] == 'C' and sum < 500:
+            sum += 100
+        elif s[i] == 'C' and sum >= 500:
+            sum -= 100
+        elif s[i] == 'D':
+            sum += 500  
+        elif s[i] == 'M':
+        	sum += 1000  
+    return sum        
 
-    for i in range(2, -1, -1):
-        for j in range(9, 0, -1):
-            # print i,j
-            # s.find(c[i][j])
-            if s.find(c[i][j]) != -1:
-                # print s.find(c[i][j])
-                # print c[i][j]
-                c_len = len(c[i][j])
-                lon = len(s)
-                s = s[c_len: lon]
-                print s
-                sumnum += 10**i * j
-                # print sumnum
-                break
-
-    # print sumnum
-    return sumnum
-
-s = "MDCCCLXXXIV"  # 1884
+s = "MMMXL"  # 1884
 a = romanToInt(s)
 print a
