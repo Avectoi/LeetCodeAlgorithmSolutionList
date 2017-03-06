e323a04b9f6279a701d1d4ee847f3b4989a5388f
Illyasviel <carson965853914@outlook.com>
2017-03-07 00:10:06 +0800

    add3036

---
+++ OJ/PF2012E1/3036.cpp
@@ -0,0 +1,20 @@
+//------------------------------------
+// Created by Carson Bright
+// The best, or nothing.
+//------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+//new func here
+
+int main()
+{
+	int a,b,x1,m,n;
+	while(cin >> a >> b >> x1 >> m >> n){
+		if (a == -1 && b == -1 && x1 == -1 && n == -1 && m == -1) break;
+		
+	}
+
+	cout << 2333 << endl;
+	return 0;
+}
