b83af1362fdde5e53e029206c7c53647086282a1
Illyasviel <carson965853914@outlook.com>
2017-03-07 00:04:41 +0800

    add3035

--- OJ/PF2012E1/3035.cpp
+++ OJ/PF2012E1/3035.cpp
@@ -6,17 +6,43 @@
 using namespace std;
 
 //new func here
+struct score
+{
+	int study_score;
+	int sutuo_score;
+};
+
 
 int main()
 {
 	int n;
+	int prize[11];
 	cin >> n;
-	int array[10001];
+	score stu[10001];
+	int total_score,study_score,sutuo_score,degree;
+	for (int i = 0; i < 11; ++i)
+	{
+		prize[i] = 0;
+	}
 	for (int i = 0; i < n; ++i)
 	{
-		cin >> array[i];
+		cin >> study_score >> sutuo_score;
+		total_score = study_score * 7 + sutuo_score * 3;
+		degree = 10 - (total_score / 111);
+		prize[degree]++;
+	}
+	int max = 0, max_num;
+	for (int i = 1; i < 11; ++i)
+	{
+		if (max < prize[i])
+		{
+			max = prize[i];
+			max_num = i;
+		}
+		/* code */
 	}
 
-	cout << endl;
+
+	cout << max_num;
 	return 0;
 }
