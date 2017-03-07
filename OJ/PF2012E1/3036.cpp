08cb73f73cfd2c3a82a69bc5df153fd13e59cfcd
Illyasviel <carson965853914@outlook.com>
2017-03-07 17:50:18 +0800

    add3036

--- OJ/PF2012E1/3036.cpp
+++ OJ/PF2012E1/3036.cpp
@@ -12,9 +12,15 @@ int main()
 	int a,b,x1,m,n;
 	while(cin >> a >> b >> x1 >> m >> n){
 		if (a == -1 && b == -1 && x1 == -1 && n == -1 && m == -1) break;
-		
+		for (int i = 0; i < n - 1; ++i)
+		{
+			x1 = (a * x1 + b) % m;
+			
+		}
+		cout << x1 << endl; 
+
 	}
 
-	cout << 2333 << endl;
+	// cout << 2333 << endl;
 	return 0;
 }
