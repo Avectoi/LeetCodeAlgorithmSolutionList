3053ac30bdcae995dc0f922d6fdd432e4ff7204d
Illyasviel <carson965853914@outlook.com>
2017-03-07 18:02:32 +0800

    change file

---
+++ OJ/201703072012ACMPD1st/1007.cpp
@@ -0,0 +1,22 @@
+//------------------------------------
+// Created by Carson Bright
+// The best, or nothing.
+//------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+//new func here
+
+int main()
+{
+	string s1,s2;
+	// cin >> s1 >> s2;
+	s1 = 12344353.323;
+	s2 = 543.21;
+	int len1 = s1.find('.');
+	int len2 = s2.find('.');
+
+
+	cout << len;
+	return 0;
+}
