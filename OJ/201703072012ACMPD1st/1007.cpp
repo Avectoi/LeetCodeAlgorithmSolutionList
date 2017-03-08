050e797e8d4870b3bc525130a3b42c37dcba62fb
Illyasviel <carson965853914@outlook.com>
2017-03-08 12:17:25 +0800

    add 1007

--- OJ/201703072012ACMPD1st/1007.cpp
+++ OJ/201703072012ACMPD1st/1007.cpp
@@ -6,17 +6,159 @@
 using namespace std;
 
 //new func here
-
+//由于形式固定，只需要从最末一位（小数点后两位）加上来就行了。
 int main()
 {
 	string s1,s2;
-	// cin >> s1 >> s2;
-	s1 = 12344353.323;
-	s2 = 543.21;
-	int len1 = s1.find('.');
-	int len2 = s2.find('.');
+	cin >> s1 >> s2;
+	// s1 = "9800.00";
+	// s2 = "2500.00";//
+
+	int sit1,sit2,len1,len2;
+	len1 = s1.length();
+	len2 = s2.length();
+	if (len1 < len2)
+	{
+		string tmp;
+		tmp = s1;
+		s1 = s2;
+		s2 = tmp;
+		int temp;
+		temp = len1;
+		len1 = len2;
+		len2 = temp;
+	}
+	// cout << len1 << ' '<<len2 << endl;
+	// cout << s1 << ' '<<s2 << endl;
+
+	int jin = 0,num1,num2,sum;
+	string sum_num;
+
+	for (int i = 1; i <= len1; ++i)
+	{
+		if (i == 3)
+		{
+			sum_num[len1 - i] = '.';
+			continue;
+		}
+		else if (i > len2)
+		{
+			if (jin == 0 )
+			{
+
+				sum_num[len1 - i] = s1[len1 - i];
+				jin = 0;
+			}
+			else{
+				if(s1[len1 - i] - '0' + 1 >= 10){
+					int s;
+					
+					jin = 1;
+					sum_num[len1 - i] = s + '0';
+				}
+				else{
+					int s;
+					s = (s1[len1 - i] - '0' + 1) % 10;
+					// cout <<s;
+					jin = 0;
+					// cout << sum_num[len1 - i] << endl;
+					sum_num[len1 - i] = s + '0';
+					// cout << sum_num[len1 - i]<< ' ' << 2333 << len1 - i<< endl;
+				}
+			}
+		}
+		else {
+			if (jin == 0)
+			{
+				num1 = s1[len1 - i] - '0';
+				num2 = s2[len2 - i] - '0';
+				sum = num1 + num2;
+				// cout << num1 << ' ' <<  num2 << ' ' << sum <<endl;
+				if (sum >= 10)
+				{
+					jin = 1;
+					sum %= 10;
+					sum_num[len1 - i] = sum + '0';
+				}
+				else{
+					jin = 0;
+					sum_num[len1 - i] = sum + '0';
+				}
+			}
+			else if (jin == 1)
+			{
+				num1 = s1[len1 - i] - '0';
+				num2 = s2[len2 - i] - '0';
+				sum = num1 + num2 + 1;
+				if (sum >= 10)
+				{
+					jin = 1;
+					sum %= 10;
+					sum_num[len1 - i] = sum + '0';
+				}
+				else{
+					jin = 0;
+					sum_num[len1 - i] = sum + '0';
+				}				
+			}
 
 
-	cout << len;
+
+		}
+	}
+	if (jin == 1)
+	{	len1++;
+		for (int i = 1; i <= len1; ++i)
+		{
+			sum_num[len1 - i] = sum_num[len1 - i - 1];
+		}
+		sum_num[0] = '1';
+
+	}
+
+
+
+
+	for (int i = 0; i < len1; ++i)
+	cout << sum_num[i];
+	cout << endl;
 	return 0;
 }
+
+
+	// // if (len1 < len2)
+	// // {
+	// // 	string tmp;
+	// // 	tmp = s1;
+	// // 	s1 = s2;
+	// // 	s2 = tmp;
+	// // 	int tmp2;
+	// // 	tmp2 = sit1;
+	// // 	sit1 = sit2;
+	// // 	sit2 = tmp2;
+	// // 	tmp2 = len1;
+	// // 	len1 = len2;
+	// // 	len2 = tmp2;
+	// // }
+
+	// string s1z,s1f,s2z,s2f,s3z,s3f;
+	// s1z = s1.substr(0,sit1);
+	// s1f = s1.substr(sit1 + 1, len1);
+	// s2z = s2.substr(0,sit2);
+	// s2f = s2.substr(sit2 + 1, len2);
+	// int max_len;
+	// max_len = max(s1z.length(),s2z.length());
+	// for (int i = max_len - 1; i >= 0; --i)
+	// {	
+	// 	if (i > s1z.length())
+	// 	{
+	// 		s3z[i] = s2z[i];
+	// 	}
+	// 	else if (i > s2z.length())
+	// 	{
+	// 		s3z[i] = s1z[i];
+	// 	}
+	// 	else{
+	// 		s3z
+	// 	}
+	// }
---
+++ OJ/201703072012ACMPD1st/1008.cpp
@@ -0,0 +1,17 @@
+//------------------------------------
+// Created by Carson Bright
+// The best, or nothing.
+//------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+//new func here
+//可利用基姆拉尔森计算公式获取星期几，然后算其他信息。
+//W= (d+2*m+3*(m+1)/5+y+y/4-y/100+y/400+1)%7 //C++计算公式
+int main()
+{
+	
+
+	cout << endl;
+	return 0;
+}
